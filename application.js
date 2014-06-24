17465685be3cf3def6231c64d9eaaa522e6e7dea
John2143658709 <Johnnyschmidt1998@gmail.com>
2014-06-23 21:47:50 -0400

    Added more slots

--- application.js
+++ application.js
@@ -1,10 +1,16 @@
-function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode ) {}
-function DownloadingFile( fileName ) {}
-function SetStatusChanged( status ) {}
 const content = $("#total")
 const left = $("#left")
+const status = $("#status")
+const fl = $("#file")
+function GameDetails( servername, serverurl, mapname, maxplayers, steamid, gamemode ) {}
+function DownloadingFile( fileName ) {
+	fl.html("Currently downloading" + fileName + "...")
+}
+function SetStatusChanged( status ) {
+	status.html(status + "...")
+}
 function SetFilesTotal( total ) {
-	content.html(total + " files left")
+	content.html(total + " files to download.")
 }
 function SetFilesNeeded( needed ) {
 	left.html(needed + " files left")
